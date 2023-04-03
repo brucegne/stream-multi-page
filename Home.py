312a15d053a708046c1752d23603cd50bd12d11a
Bruce Gordon <brucegne@gmail.com>
2023-04-03 10:07:49 -0500

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.header("Welcome to one and all")
+
+st.write("---")
+
+st.write("Well, is there a line abouve this?")
+"""
+
+# This is a big Heading
+## This shoud be a little *Smaller

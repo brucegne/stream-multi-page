bc6fda5102971bf8c00a5418a848995225b4bede
Bruce Gordon <brucegne@gmail.com>
2023-04-03 13:25:54 -0500

    try the camera

--- pages/cols.py
+++ pages/cols.py
@@ -8,11 +8,14 @@ with col1:
 
 with col2:
     col2.header("This should be in the center")
+    my_upload = st.file_uploader("Upload a local photo")
     my_image = st.camera_input("Take your picture")
 
 with col3:
     col3.header("This is hanging out on the right")
     if my_image:
         st.write(my_image)
-        
+    if my_upload:
+        st.write(my_upload)
+
 

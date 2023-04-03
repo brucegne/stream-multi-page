5709527500fd5d5b97034fbd9d45b69bff086718
Bruce Gordon <brucegne@gmail.com>
2023-04-03 16:20:23 -0500

    No Sorting yet

--- Home.py
+++ Home.py
@@ -50,7 +50,7 @@ recs = response.json()
 result = json.dumps(recs['records'], 
 
 df3 = pd.DataFrame(results)
-st.write(df3.sort_values(by=['location']))
+st.write(df3.sort_values(by='location'))
 
 for i in range(25):
     rec = results[i]

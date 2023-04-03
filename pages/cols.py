1e02e2a69d42b63a1ba9c3a5b6cde2ec0354d5d2
Bruce Gordon <brucegne@gmail.com>
2023-04-03 13:16:02 -0500

    try some columns

---
+++ pages/cols.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+col1, col2, col3 = st.columns(1,2,1)
+
+with col1:
+    col1.header("This is on the left")
+    col1.subheader("This would be a little smaller")
+
+with col2:
+    col2.header("This should be in the center")
+
+with col3:
+    col3.header("This is hanging out on the right")
+

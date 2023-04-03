3305bbdc5973a86b9f0028ab53d75f70f16bc0f3
Bruce Gordon <brucegne@gmail.com>
2023-04-03 10:45:20 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,16 +1,55 @@
 import streamlit as st
+import gspread
 import pandas as pd
 import numpy as np
 
-st.header("Welcome to one and all")
+report_line = ['And a 1', 'And a 2', 'Time', 'And a 3']
 
-st.write("---")
+gc = gspread.service_account("secure.json")
 
-st.write("Well, is there a line abouve this?")
-"""
+sh = gc.open("WeddingApp")
 
-# This is a big Heading
-## This shoud be a little *Smaller
+# ws2 = sh.add_worksheet(title="System", rows=10, cols=5)
 
-"""
-st.write("---")
+ws = sh.worksheet("Attractions")
+
+# ws.update("B4", "Wedding")
+
+try:
+    cell = ws.find("KEY101")
+    if cell:
+        st.write(cell.row, cell.col)
+        st.write("Found something at R%sC%s" % (cell.row, cell.col))
+    else:
+        st.write("Doesn't exist")
+except:
+    st.write("Key value not found")
+    
+st.write(ws.get('B4'))
+
+st.write(ws.acell("B4").value)
+
+# ws.update("B4", "Bingo !!!")
+
+# print(sh.worksheets())
+
+st.write(ws.row_values(3)[0])
+
+# print(sh.sheet1.row_values(4))
+
+# df = pd.DataFrame(ws.get_all_records())
+
+# pprint.pprint(df)
+
+res = ws.get_all_records()
+
+for rec in res:
+    st.write(rec['Name'])
+    
+# ws.append_row(report_line)
+
+array = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Write the array to worksheet starting from the A2 cell
+# ws.update('A12', array.tolist())
+st.write(ws.get_all_records())

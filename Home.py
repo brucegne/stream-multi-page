6b1cea269d91572e561156de971abf779b1323b4
Bruce Gordon <brucegne@gmail.com>
2023-04-03 17:02:43 -0500

    Finally got it soring

--- Home.py
+++ Home.py
@@ -50,7 +50,7 @@ recs = response.json()
 results = recs['records']
 
 df3 = pd.DataFrame(results)
-df3 = df3.sort_values(by=['buyer','location'])
+df3 = df3.sort_values(by=['buyer','location'])[['buyer','location']]
 st.write(df3.head(45))
 
 for i in range(25):

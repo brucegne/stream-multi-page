17adb7c4b61a5f235f435d393a950f7f4deaa0a7
Bruce Gordon <brucegne@gmail.com>
2023-04-03 19:29:52 -0500

    Finally got it soring

--- Home.py
+++ Home.py
@@ -51,7 +51,7 @@ results = recs['records']
 
 df3 = pd.DataFrame(results)
 df3 = df3.sort_values(by=['commodity','buyer','location'])[['buyer','location','commodity','basis']]
-st.write(df3.basis > 0)
+st.write(df3.commodity == 'Corn')
 st.write(df3.head(50))
 
 df3.plot(kind='scatter',x='buyer', y='basis')

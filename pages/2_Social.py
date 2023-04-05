85fa939e14c7f5594fff7b0b92eb223b06852b6a
Bruce Gordon <brucegne@gmail.com>
2023-04-05 08:15:04 -0500

    Dev step

--- pages/2_Social.py
+++ pages/2_Social.py
@@ -7,3 +7,8 @@ import time, json, requests
 
 work_url = 'https://socialpancakes-d1dad.firebaseio.com/Users.json'
 
+results = requests.get(work_url)
+res = results.json()
+for rec in res:
+    st.write(res[rec]['age'],res[rec]['name'])
+    

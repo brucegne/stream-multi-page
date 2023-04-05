cb3213df1c7f1c19933a1b27b31ede0e56b9f52a
Bruce Gordon <brucegne@gmail.com>
2023-04-05 08:38:38 -0500

    Dev step

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests, json
 
-st.set_page_config(page_title="Just messing around",
+st.set_page_config(page_title="Nifty Stuff",
                     page_icon=":bar_chart:",
                     layout="wide")
 

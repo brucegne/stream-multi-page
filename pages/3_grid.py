8a830fc8a677f4ff44bb271ea13911b22ebb2ca0
Bruce Gordon <brucegne@gmail.com>
2023-04-05 09:31:13 -0500

    Dev step

--- pages/3_grid.py
+++ pages/3_grid.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 from openpyxl import Workbook
+from openpyxl import load_workbook
 
 workbook = Workbook()
 sheet = workbook.active
@@ -11,3 +12,13 @@ sheet["B1"] = "world!"
 
 workbook.save(filename="hello_world.xlsx")
 st.write("It worked!")
+
+workbook = load_workbook(filename="hello_world.xlsx")
+st.write(workbook.sheetnames)
+
+
+sheet = workbook.active
+st.write(sheet)
+
+
+st.write(sheet.title)
